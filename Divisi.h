dba395c568f9ed53c735b8d27cebb07f11cd5fdf
DitRadit <adiradityagt@gmail.com>
2025-11-28 12:05:36 +0700

    update header divisi dan subprogram

--- Divisi.h
+++ Divisi.h
@@ -8,23 +8,37 @@ using namespace std;
 typedef struct divisi infotypeDivisi;
 typedef struct elmListDivisi *adrDivisi;
 
-struct elmListDivisi {
+struct elmListDivisi
+{
     infotypeDivisi infoD;
     adrDivisi next;
     adrDivisi prev;
     adrPegawai firstPegawai;
 };
 
-struct divisi{
+struct divisi
+{
     string nama;
     int idDivisi;
     int jumlahPegawai;
     string lokasiDivisi;
 };
 
-struct listDivisi{
+struct listDivisi
+{
     adrDivisi first;
     adrDivisi last;
 };
+
+void createListDivisi(listDivisi &L);
+bool isEmptyDivisi(listDivisi L);
+adrDivisi createElmDivisi(infotypeDivisi x);
+void insertFirstDivisi(listDivisi &L, adrDivisi P);
+void insertLastDivisi(listDivisi &L, adrDivisi P);
+void deleteFirstDivisi(listDivisi &L, adrDivisi &P);
+void deleteLastDivisi(listDivisi &L, adrDivisi &P);
+adrDivisi findElmDivisi(listDivisi L, int idDivisi);
+void printInfoDivisi(listDivisi L);
+void printPegawaiByDivisi(adrDivisi D);
 
 #endif // DIVISI_H_INCLUDED
